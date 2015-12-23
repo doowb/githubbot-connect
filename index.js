dadb28f2a55d099757a1f832a739e15aa3bdca9b
Brian Woodward <brian.woodward@gmail.com>
2015-12-23 18:59:52 -0500

    implementation

--- index.js
+++ index.js
@@ -7,6 +7,57 @@
 
 'use strict';
 
-module.exports = function () {
-  // do stuff
+/**
+ * githubbot plugin that adds a `.middleware` method to githubbot bots that enables creating
+ * connect/express middleware functions to handle github webhook event payloads.
+ *
+ * ```js
+ * bot.use(require('githubbot-connect')(options));
+ * ```
+ *
+ * @param  {Object} `options` Options to configure the plugin.
+ * @return {Function} plugin function to pass to `.use`
+ * @api public
+ * @name githubbot-connect
+ */
+
+module.exports = function (options) {
+  return function plugin(bot) {
+
+    /**
+     * Create a middleware function suited to pass to a connect/express route.
+     * Middleware function will inspect the request headers to determine which
+     * github webhook event should be handled.
+     *
+     * ```js
+     * app.post('/webhooks', bot.middleware(options));
+     * ```
+     *
+     * @param  {Object} `opts` Options to configure how events should be handled.
+     * @param  {Boolean} `opts.send` When `true`, the results of handling the payload will automatically be sent using `res.json()`. Defaults to `false`.
+     * @return {Function} Middleware function for use in connect/express routes.
+     * @api public
+     * @name middleware
+     */
+
+    bot.define('middleware', function (opts) {
+      opts = opts || {};
+      return function(req, res, next) {
+        var event = req.headers['x-github-event'];
+        if (!event || bot.events.indexOf(event) === -1) {
+          return next();
+        }
+
+        // TODO: payload parsing options
+        var payload = req.body;
+        bot.handle(event, payload, function(err, results) {
+          if (err) return next(err);
+          if (opts.send === true) {
+            return res.json(results);
+          }
+          next(null, results);
+        });
+      };
+    });
+  };
 };
